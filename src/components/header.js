95d37f22fbc9809b9e2635542b646937b2be16cc
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-04-16 14:42:46 -0400

    created/styled header & made it responsive

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,7 @@
 import Sidebar from "./Sidebar";
 import { AvatarGenerator } from "random-avatar-generator";
+import logo from "../assets/images/loading-video.png";
+import { AiOutlineSearch } from "react-icons/ai";
 import "./header.scss";
 
 const Header = () => {
@@ -8,19 +10,39 @@ const Header = () => {
   return (
     <>
       <div className="header">
+        <ul className="header__desktop-navbar">
+          <img src={logo} className="header__desktop-logo"></img>
+          <a href="/" className="header__desktop-recipes">
+            Recipe Category
+          </a>
+
+          <a href="/" className="header__desktop-ingredients">
+            Ingredients
+          </a>
+          <a href="/" className="header__desktop-countries">
+            Countries
+          </a>
+        </ul>
+        <ul className="header__nav">
+          <input
+            className="header__desktop-search"
+            type="search"
+            placeholder=" search"
+          ></input>
+          <img
+            className="header__profile"
+            src={profilePic}
+            alt="Profile "
+          ></img>
+          <button className="header__desktop-signup">Sign Up</button>
+          <button className="header__desktop-signup">Log In</button>
+        </ul>
+      </div>
+      <div className="header__sidebar">
         <Sidebar
           pageWrapId={"page-wrap"}
           outerContainerID={"outer-container"}
         />
-        <div className="header">
-          <nav className="header__nav">
-            <img
-              className="header__profile"
-              src={profilePic}
-              alt="Profile "
-            ></img>
-          </nav>
-        </div>
       </div>
     </>
   );
