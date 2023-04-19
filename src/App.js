5bec84eaaf4d737f07fc642961f0c299a293d0fa
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-04-19 14:40:42 -0400

    added color styles  globally for copyright element

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.scss";
 import Header from "./components/header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Header />
+        <Footer />
       </BrowserRouter>
     </>
   );
