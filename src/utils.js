0e809d1e1d0ab9880ab1322ded78a5b9ba4f785e
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-04-30 23:19:44 -0400

    added more api urls

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,11 @@
 export const categoryApiUrl =
   "https://www.themealdb.com/api/json/v1/1/categories.php";
+
+export const randomMealUrl =
+  "http://www.themealdb.com/api/json/v1/1/random.php";
+
+export const mealsByCountryUrl =
+  "http://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian";
+
+export const searchByLetter =
+  "https://www.themealdb.com/api/json/v1/1/search.php?f";
