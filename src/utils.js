a3e73f57146f3e5c9b8c2609a6c151fdbc6e4708
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-05-06 01:43:48 -0400

    added more api urls for fetch requests

--- src/utils.js
+++ src/utils.js
@@ -5,10 +5,13 @@ export const randomMealUrl =
   "http://www.themealdb.com/api/json/v1/1/random.php";
 
 export const mealsByCountryUrl =
-  "http://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian";
+  "http://www.themealdb.com/api/json/v1/1/filter.php?a=";
 
 export const searchByLetter =
   "https://www.themealdb.com/api/json/v1/1/search.php?f=";
 
 export const searchById =
   "http://www.themealdb.com/api/json/v1/1/lookup.php?i=";
+
+export const searchByCategory =
+  "https://www.themealdb.com/api/json/v1/1/filter.php?c=";
