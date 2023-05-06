ebec32ab22578c6f8da63fef5944861c6e741a34
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-05-06 01:43:05 -0400

    added route for ingredient search page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUpPage from "./Pages/Page/SignUp";
 import HomePage from "./Pages/Page/homepage/HomePage";
+import CategoryPage from "./Pages/Page/categorypage/CategoriesPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/:ingredients" element={<CategoryPage />} />
           <Route path="/signup" element={<SignUpPage />} />
         </Routes>
       </BrowserRouter>
