1b948491dccf68ca2021b72c473245bc6ecb7e7f
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-05-08 23:32:07 -0400

    added back arrow to ui

--- src/components/recipesList/RecipesList.js
+++ src/components/recipesList/RecipesList.js
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
+import { AiOutlineArrowLeft } from "react-icons/ai";
 import RecipeCard from "../recipeCard/RecipeCard";
 import "./RecipesList.scss";
 
 function RecipesList(recipes) {
   const recipeData = recipes.recipes.meals;
-
   return (
     <section className="recipe-list">
+      <Link to={`/`}>
+        <div className="recipe-directions__back-arrow">
+          <AiOutlineArrowLeft className="" />
+        </div>
+      </Link>
       <div className="recipe-list__container">
         {recipeData.map((recipeData) => (
           <Link to={`${recipeData.idMeal}`}>
