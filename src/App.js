7e2187cc5cc1012c2b596e9cc81fe949392c72ea
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-05-09 10:17:13 -0400

    added login page & search by country route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SignUpPage from "./Pages/Page/SignUp";
+import SignUpPage from "./Pages/Page/signuppage/SignUp";
 import HomePage from "./Pages/Page/homepage/HomePage";
 import CategoryPage from "./Pages/Page/categorypage/CategoriesPage";
 import RecipePage from "./Pages/Page/recipe-page/RecipePage";
+import SearchByCountry from "./Pages/Page/filterbycountry/SearchByCountry";
+import LoginPage from "./Pages/Page/loginpage/LoginPage";
 
 function App() {
   return (
@@ -11,9 +13,11 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/country" element={<SearchByCountry />} />
           <Route path="/:categories" element={<CategoryPage />} />
           <Route path=":categories/:id" element={<RecipePage />} />
           <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
       </BrowserRouter>
     </>
