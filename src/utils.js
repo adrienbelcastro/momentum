0280d54a390d01382ee2fe308ee9065f4b60f63b
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-05-09 22:29:29 -0400

    fixed incorrect api url

--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,7 @@ export const randomMealUrl =
   "http://www.themealdb.com/api/json/v1/1/random.php";
 
 export const mealsByCountryUrl =
-  "http://www.themealdb.com/api/json/v1/1/filter.php?a=";
+  "https://www.themealdb.com/api/json/v1/1/filter.php?a=";
 
 export const searchByLetter =
   "https://www.themealdb.com/api/json/v1/1/search.php?f=";
