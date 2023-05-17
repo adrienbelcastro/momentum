a4bafabf863541eba406dbe483c0a6d8a142461b
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-05-16 20:11:11 -0400

    Created meal planner page

---
+++ src/Pages/Page/mealplanner/MealPlanner.js
@@ -0,0 +1,48 @@
+import Header from "../../../Components/header/header";
+import Footer from "../../../Components/footer/footer";
+import "./MealPlanner.scss";
+import Diary from "../../../Components/diary/Diary";
+
+function MealPlanner({ planner }) {
+  console.log(planner.breakfast);
+  return (
+    <>
+      <Header />
+      <section className="planner">
+        <div className="planner__tracker">
+          <h1 className="planner__total-calories">
+            {planner.calories.current}/{planner.calories.total} Calories
+          </h1>
+        </div>
+
+        {planner.breakfast.map((meal, index) => (
+          <div key={index} className="planner__container">
+            <h1>Breakfast</h1>
+            <Diary meal={meal} />
+          </div>
+        ))}
+        {planner.lunch.map((meal, index) => (
+          <div key={index} className="planner__container">
+            <h1>Lunch</h1>
+            <Diary meal={meal} />
+          </div>
+        ))}
+        {planner.dinner.map((meal, index) => (
+          <div key={index} className="planner__container">
+            <h1>Dinner</h1>
+            <Diary meal={meal} />
+          </div>
+        ))}
+        {planner.snack.map((meal, index) => (
+          <div key={index} className="planner__container">
+            <h1>Snack</h1>
+            <Diary meal={meal} />
+          </div>
+        ))}
+      </section>
+      <Footer />
+    </>
+  );
+}
+
+export default MealPlanner;
