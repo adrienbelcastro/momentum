d3d8948cf7105a2c47dd3fabd0d6b930fa12822a
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-07-05 15:05:08 -0400

    created article list component

--- src/components/articles/ArticleList.js
+++ src/components/articles/ArticleList.js
@@ -1,19 +1,44 @@
-import React from "react";
-import image from "../../assets/images/signup-background.jpg";
+import React, { useState, useEffect } from "react";
+
+import { nutritionDatabaseURL } from "../../utils";
+import axios from "axios";
 import "../articles/ArticleList.scss";
+import ArticleCard from "./ArticleCard";
+import { Link } from "react-router-dom";
+import Loading from "../loading/Loading";
 
 export default function ArticleList() {
-  return (
-    <div className="article-list">
-      <img
-        className="article-list__thumbnail"
-        src={image}
-        alt="Article Thumbnail"
-      />
-      <div>
-        <h1 className="article-list__title">Lorem Ipsum All Day</h1>
-        <h3 className="article-list__author">Bob The Builder</h3>
+  const [articles, setArticles] = useState();
+
+  useEffect(() => {
+    axios
+      .get(`${nutritionDatabaseURL}article`)
+      .then((result) => {
+        setArticles(result.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  if (!articles) {
+    return <Loading />;
+  } else {
+    return (
+      <div className="article-list">
+        <h1 className="article-list__title">Popular Articles</h1>
+        <div className="article-list__container">
+          {articles.map((articles, index) => (
+            <Link key={index} to={`/article/${articles.id}`}>
+              <ArticleCard
+                title={articles.title}
+                image={articles.image}
+                author={articles.author}
+              />
+            </Link>
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
