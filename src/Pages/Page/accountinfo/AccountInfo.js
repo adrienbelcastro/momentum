223b727cf213813b297a02482043cca978ca54b1
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-08-06 02:07:39 -0400

    Added form component to account info page

---
+++ src/Pages/Page/accountinfo/AccountInfo.js
@@ -0,0 +1,10 @@
+import React from "react";
+import AccountFrom from "../../../components/account/AccountFrom";
+
+export default function AccountInfo() {
+  return (
+    <div>
+      <AccountFrom />
+    </div>
+  );
+}
