a33ff5ec16079baa781bd034b847c3f1a52434db
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-08-06 15:14:11 -0400

    added header to account info page

--- src/Pages/Page/accountinfo/AccountInfo.js
+++ src/Pages/Page/accountinfo/AccountInfo.js
@@ -1,9 +1,11 @@
 import React from "react";
 import AccountFrom from "../../../components/account/AccountFrom";
+import Header from "../../../components/header/header";
 
 export default function AccountInfo() {
   return (
     <div>
+      <Header />
       <AccountFrom />
     </div>
   );
