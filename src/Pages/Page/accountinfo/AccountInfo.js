74f99090724c9108752b4a63f2a52969bf0c97b0
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-09-27 16:43:54 -0400

    added in a title and a subtitle

--- src/Pages/Page/accountinfo/AccountInfo.js
+++ src/Pages/Page/accountinfo/AccountInfo.js
@@ -1,12 +1,21 @@
 import React from "react";
-import AccountFrom from "../../../components/account/AccountFrom";
+import EditAccountForm from "../../../components/editaccountform/EditAccountForm";
 import Header from "../../../components/header/header";
+import "./AccountInfo.scss";
 
-export default function AccountInfo() {
+export default function AccountInfo(isLoggedIn) {
   return (
-    <div>
-      <Header />
-      <AccountFrom />
-    </div>
+    <>
+      <Header isLoggedIn={isLoggedIn} />
+      <div className="account">
+        <h1 className="account__title">Account Info</h1>
+        <p className="account__description">
+          Manage your account info including name, age and email address.
+        </p>
+        <div className="account__container">
+          <EditAccountForm />
+        </div>
+      </div>
+    </>
   );
 }
