f65ef8c28cf9ec57111d453592ce52cc38d042bd
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-10-06 13:44:25 -0400

    created sign up page

--- src/Pages/Page/signuppage/SignUp.js
+++ src/Pages/Page/signuppage/SignUp.js
@@ -1,11 +1,19 @@
-import SignUpForm from "../../../components/signupform/SignUpForm";
+import AccountForm from "../../../components/authenticationform/AuthenticationForm";
+import { redirect } from "react-router-dom";
 import "./SignUp.scss";
 
-export default function SignUpPage() {
+export default function SignUpPage({ isLoggedIn, setIsLoggedIn }) {
+  if (isLoggedIn) {
+    return redirect("/");
+  }
   return (
     <>
       <div className="signup">
-        <SignUpForm />
+        <div className="signup__container">
+          <h2 className="form__subheader">Create An Account</h2>
+          <h4 className="form__tagline">No More Boxed Mac And Cheese</h4>
+        </div>
+        <AccountForm setIsLoggedIn={setIsLoggedIn} />
       </div>
     </>
   );
